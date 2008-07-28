e6cfb61245755a2e320961d31189f30b8f2917e0
Arthur Foelsche <arthurf@27259.no-reply.drupal.org>
2008-07-28 17:03:39 +0000

    major update that contains the new javascript form updating with config files. does caching of default configurations and then provides any output format with either the default data or the conf file in the conf directory. Pretty neat little gizmo

--- README.txt
+++ README.txt
@@ -10,7 +10,7 @@ Make sure you configure the path to FFmpeg. This is relative to the root
 of your server. If don't know where FFmpeg is installed and you have
 access to the command line of your server, you can run:
 
-#locate ffmpeg
+$locate ffmpeg
 
 This should hopefully give you a path to ffmpeg. 
 If you need install FFmpeg, there are instructions here for installing 
@@ -19,3 +19,29 @@ it on a debian/ubuntu environment:
 http://www.24b6.net/?p=188
 
 If you need a compiled binary, check http://mediamover.24b6.net/ffmpeg
+
+-----------------------------------------------------------------------
+ DEVELOPERS
+-----------------------------------------------------------------------
+ffmpeg_wrapper supports defining configurations that set default values
+in forms that are built to support ffmpeg_wrapper. Look in the conf dir
+for an example of how these configurations can be built. This is helpful
+for constraining users from building configurations that ffmpeg will not
+function with.
+
+To enable the support in your forms, merely call this function: 
+  ffmpeg_wrapper_enable($prefix, $output_form_id);
+
+Here, $prefix is a prefix used in your forms (in standard drupal, this will
+be at least "edit-"). $output_form_id is the name of the output controler
+form element. Check out media mover API's mm_ffmpeg module for an example.
+
+The second piece that needs to be done is to name your form elements with
+the ffmpeg_wrapper convention. Any form element that controls an aspect
+of ffmpeg (eg, audio bit rate) needs to be named in a specific way:
+  ffmpeg_audio_ab 
+Where ffmpeg is always used, audio is for audio options (video is the other
+possibility) and ab is the ffmpeg command for setting the audio bit rate.
+Again, mm_ffmpeg offers a good example of how to do this.
+
+
--- ffmpeg_wrapper.js
+++ ffmpeg_wrapper.js
@@ -1,65 +1,58 @@
 // $Id$;
 
-function ffmpeg_wrapper_update_options(output, prefix) {
-  
+/**
+ * @param string $prefix is the prefix of the elements
+ * @param string $source is the source for the output type
+ */
+function ffmpeg_wrapper_update_options(prefix, source) {
+
   // get the path to the output settings set in ffmpeg_wrapper module
   var path = Drupal.settings.ffmpeg_wrapper.ffmpeg_wrapper_output_url;
   
-  // now get the values for this codec
-  $.getJSON(path+output, function(json) {
+  // get the output type
+  var output = $('#'+prefix+source).val();
   
-    // now we need to itterate through each of the keys returned and 
-    // limit the choices to the incoming values.
-    var data = eval (json);
+  // only look for the value if there is one selected
+  if (output != 0) {
+    // now get the values for this codec
+    $.getJSON(path+output, function(json) {
     
-    // first break the configuration into the types of config data we have
-    for (var type in data) {
-      // now get each of the items in the config      
-      for (var key in data[type] ) {
-       // get the element
-       var element = '#'+prefix+'-'+type+'-'+key;
-       //var element = '#edit-process--mm-ffmpeg--1--ffmpeg-'+type+'-'+key;
-       // make sure element exists
-       if ($(element)) {
-         // remove existing items   
-         $(element).html('');
-         var html = '';
-         for (var option in data[type][key]) {
-           html += '<option value="'+data[type][key][option]+'">'+data[type][key][option]+'</option>';
-         }
-         $(element).html(html);
-        }
-      }
-    }
-  });
-}
+      // now we need to itterate through each of the keys returned and 
+      // limit the choices to the incoming values.
+      var data = eval (json);
+      
+      // first break the configuration into the types of config data we have
+      for (var type in data) {
+        // catch the flag for default settings and don't do 
+        // any form updating for this value
+        if (data[type] != 'default') {
+          // now get each of the items in the config      
+          for (var key in data[type] ) {
+           // build the element from the prefix value, ffmpeg, the kind of item it is, and the key 
+           var element = '#'+prefix+'ffmpeg-'+type+'-'+key;
   
-
-
-}
-
-$.getJSON('http://localhost/5.7js/ffmpeg_wrapper/codec/vlc',
-  function(json){
-    var data = eval (json);
-    // first break the configuration
-    // into the types of config data we have
-    for (var type in data) {
-      // now get each of the items in the config      
-      for (var key in data[type] ) {
-       // get the element
-       // var element = '#'+prefix+'-'+type+'-'+key;
-       var element = '#edit-process--mm-ffmpeg--1--ffmpeg-'+type+'-'+key;
-       // make sure element exists
-       if ($(element)) {
-         // remove existing items   
-         $(element).html('');
-         var html = '';
-         for (var option in data[type][key]) {
-           html += '<option value="'+data[type][key][option]+'">'+data[type][key][option]+'</option>';
-         }
-         $(element).html(html);
+           // make sure element exists
+           if ($(element)) {
+             // remove existing items   
+             $(element).html('');
+             var html = '';
+             for (var option in data[type][key]) {
+               html += '<option value="'+data[type][key][option]+'">'+data[type][key][option]+'</option>';
+             }
+             $(element).html(html);
+            }
+          }
+          // now turn on the advanced options so they are picked up
+          $('#'+prefix+'ffmpeg-'+type+'-advanced').attr('checked', true);
+        }            
+        else {
+          // is the configuration data being passed back was the default data?
+          // turn off the advanced configuration so that nothing nasty 
+          // happens without user action
+          $('#'+prefix+'ffmpeg-audio-advanced').attr('checked', false);
+          $('#'+prefix+'ffmpeg-video-advanced').attr('checked', false);        
         }
       }
-    }
+    });
   }
-);
+}
